c308f5815a3c0849275d2a83d236bb7051b0fa11
admin <samarsinghthakur01146@outlook.com>
2024-03-06 22:16:12 +0530

    code

--- chat/models.py
+++ chat/models.py
@@ -9,7 +9,8 @@ class Room(models.Model):
     mytimestamp = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
-        return str(self.room_id) 
+        return str(self.room_id)
+    
 
 class ChatBox(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='sender_id')
